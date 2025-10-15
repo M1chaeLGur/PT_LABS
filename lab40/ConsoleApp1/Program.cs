efb7a4025af5db1aba1ee6d8789e84fd53496831
Gurin Michael <gurinmishazzz@gmail.com>
2025-10-15 18:55:50 +0300

    screenshots're added

---
+++ lab40/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuadraticEquation eq1 = new QuadraticEquation(1, -4, 4);
+            eq1.PrintInfo();
+            Console.WriteLine(eq1.GetRootsCount());
+
+
+            QuadraticEquation eq2 = new QuadraticEquation(2, 4, -5);
+            eq2.PrintInfo();
+            Console.WriteLine(eq2.GetRootsCount());
+
+            QuadraticEquation eq3 = new QuadraticEquation(1, 2, 3);
+            eq3.PrintInfo();
+            Console.WriteLine(eq3.GetRootsCount());
+
+            Console.WriteLine("get Roots of equation 1: ");
+            foreach (double root in eq1.GetRoots())
+            {
+                Console.WriteLine($"Root = {root} ");
+            }
+
+            foreach (double root in eq2.GetRoots())
+            {
+                Console.WriteLine($"Root = {root} ");
+            }
+
+            Console.WriteLine("get Roots of equation 3: ");
+            foreach (double root in eq3.GetRoots())
+            {
+                Console.WriteLine($"Root = {root} ");
+            }
+
+        }
+    }
+}
---
+++ lab40/ConsoleApp1/qe.cs
@@ -0,0 +1,57 @@
+using System;
+
+namespace ConsoleApp1
+{
+    public class QuadraticEquation
+    {
+        private int a;
+
+
+        public QuadraticEquation(int a, int b, int c)
+        {
+            this.A = a;
+            this.B = b;
+            this.C = c;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Equation: {A}x2 + {B}x + {C} = 0");
+        }
+
+        public int A
+        {
+            get { return this.a; }
+            set
+            {
+                if (value == 0)
+                {
+                    Console.WriteLine("Value of a must be != 0");
+                }
+                else
+                {
+                    this.a = value;
+                }
+            }
+        }
+
+        public int B { get; set; }
+        public int C { get; set; }
+
+        public int GetRootsCount()
+        {
+            int D = B * B - 4 * A * C;
+            if (D > 0)
+            {
+                return 2;
+            }
+            else if (D == 0)
+            {
+                return 1;
+            }
+            return 0;
+
+        }
+
+    }
+}
